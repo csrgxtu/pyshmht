37dd820f466dd066f157fd4990ed8e7039da379f
muyakongali <muyakongali@tencent.com>
2021-08-19 09:17:50 +0800

    pyshmht3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import os
-#from distutils.core import setup, Extension
+from distutils.core import setup, Extension
 from setuptools import setup, Extension
 shmht = Extension('shmht',
     sources = ['shmht.c', 'hashtable.c'],
@@ -8,14 +8,14 @@ shmht = Extension('shmht',
 )
 
 setup(
-    name            = 'pyshmht',
-    version         = '0.0.2',
-    author          = 'felix021',
-    author_email    = 'felix021@gmail.com',
-    description     = 'provide sharing memory based hash table for python',
+    name            = 'pyshmht3',
+    version         = '0.0.1',
+    author          = 'minyakonga,felix021',
+    author_email    = 'minyakonga@gmail.com',
+    description     = 'provide sharing memory based hash table for python, one process write, others read',
     license         = "BSD",
     keywords        = "python extension sharing memory based hash table",
-    url             = "http://github.com/felix021/pyshmht",
+    url             = "http://github.com/csrgxtu/pyshmht",
     ext_modules     = [shmht],
-    packages        = ["pyshmht"]
+    packages        = ["pyshmht3"]
 )
